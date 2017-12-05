2e7cf7ae58eee593d4d13a8e3949414ce657d390
Gustaf Räntilä <g.rantila@gmail.com>
2017-12-05 10:20:08 +0100

    0.0.13

--- dist/lib/generated/version.d.ts
+++ dist/lib/generated/version.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "0.0.12";
+export declare const version = "0.0.13";
--- dist/lib/generated/version.js
+++ dist/lib/generated/version.js
@@ -1,4 +1,4 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = '0.0.12';
+exports.version = '0.0.13';
 //# sourceMappingURL=version.js.map
