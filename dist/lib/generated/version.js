3009bb45416849d1dc14e812082574bc76db0415
Gustaf Räntilä <g.rantila@gmail.com>
2018-11-28 23:20:18 +0100

    fix(package): Don't ship TS, don't store dist/ in repo

--- dist/lib/generated/version.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = '1.0.0';
-//# sourceMappingURL=version.js.map
--- dist/test/lib/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function createIntegrity(data: string, hashType?: string): string;
